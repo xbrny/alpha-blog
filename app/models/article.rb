f8db6bafc2d5ef6d10e21cc26f9e3f549c1d8473
xbrny <lasermouse2@gmail.com>
2017-06-04 13:03:31 +0800

    add user validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: {minimum: 5}
-  validates :description, presence: true, length: {minimum: 10}
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 10 }
+
+  belongs_to :user
 
 end
