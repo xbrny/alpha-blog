416b7613490abd0192c75a508e3e3ab31815a064
xbrny <lasermouse2@gmail.com>
2017-06-04 18:31:07 +0800

    Create user listing and add user info in show article

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
 
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    #gravatar_size = size
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.username, class: "img-circle")
   end
