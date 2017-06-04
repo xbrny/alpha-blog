a673229ae3d22ac8102970931bea727ede4cf5bf
xbrny <lasermouse2@gmail.com>
2017-06-04 15:04:27 +0800

    Create user signup UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "welcome#index"
+
   get "about", to: "welcome#about"
   resources :articles
+
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
+
 end
