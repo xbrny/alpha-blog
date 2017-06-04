3bc58995e2df91b8b2f93ca70b5e26cf6dd00ee8
xbrny <lasermouse2@gmail.com>
2017-06-04 21:21:34 +0800

    Add sessions controller new create destroy, and login UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfull logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Something is wrong with your login information"
+      render "new"
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
